ca7adcef6b41ff58eb2828aeaf0f5354bb817498
KathirvelMV <kathirvel.mv.2017.cse@rajalakshmi.edu.in>
2025-05-03 22:29:41 -0700

    Working code push - M13L1

---
+++ M13L1/public/employees.js
@@ -0,0 +1,14 @@
+let sally = 'Sally Smith';
+let mark = 'Mark Martin';
+let holly = 'Holly Unlikely';
+let amol = 'Amol Shookup';
+let robin = 'Robin A';
+let aron = 'Aron B';
+let Ted = 'Ted Mosby';
+const element = /*#__PURE__*/React.createElement("ul", {
+  style: {
+    'color': 'blue',
+    'fontSize': '24px'
+  }
+}, /*#__PURE__*/React.createElement("li", null, sally), /*#__PURE__*/React.createElement("li", null, mark), /*#__PURE__*/React.createElement("li", null, holly), /*#__PURE__*/React.createElement("li", null, amol), /*#__PURE__*/React.createElement("li", null, robin), /*#__PURE__*/React.createElement("li", null, aron), /*#__PURE__*/React.createElement("li", null, Ted));
+ReactDOM.render(element, document.getElementById('content'));
